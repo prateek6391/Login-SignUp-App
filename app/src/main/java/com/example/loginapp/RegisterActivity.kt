5ee698db75e4941c3084b2fde11b01de5fc49fb8
unknown <prateekkasaudhan123@gmail.com>
2022-06-01 22:57:42 +0530

    Day2 -> working on xml and kotlin

--- app/src/main/java/com/example/loginapp/RegisterActivity.kt
+++ app/src/main/java/com/example/loginapp/RegisterActivity.kt
@@ -1,55 +1,46 @@
 package com.example.loginapp
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_register.*
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_register)
         auth = FirebaseAuth.getInstance()
-        login.setOnClickListener {
+        continue_btn.setOnClickListener {
             if(checking()){
-                val email = editTextEmail.text.toString()
-                val password = editTextPassword.text.toString()
-                auth.signInWithEmailAndPassword(email, password)
+                val name_reg = name_ET.text.toString()
+                val number_reg = number_ET.text.toString()
+                val email_reg = email_ET.text.toString()
+                val password_reg = password_ET.text.toString()
+                auth.createUserWithEmailAndPassword(email_reg, password_reg)
                     .addOnCompleteListener(this) { task ->
                         if (task.isSuccessful) {
                             // Sign in success, update UI with the signed-in user's information
-                            val intent = Intent(this, LoggedIn::class.java)
-                            startActivity(intent)
+                            Toast.makeText(baseContext, "User Added", Toast.LENGTH_SHORT).show()
+
                         } else {
                             // If sign in fails, display a message to the user.
-                            Toast.makeText(this, "Authentication failed.",
-                                Toast.LENGTH_SHORT).show()
+                            Toast.makeText(baseContext, "User not Added", Toast.LENGTH_SHORT).show()
                         }
                     }
             }
             else{
                 Toast.makeText(this, "Enter The Details", Toast.LENGTH_LONG).show()
             }
-        }
 
-
-
-
-
-        register.setOnClickListener {
-            val regIntent = Intent(this, RegisterActivity::class.java)
-            startActivity(regIntent)
         }
 
     }
 
     private fun checking(): Boolean {
-        if(editTextEmail.text.toString().trim{it<=' '}.isNotEmpty() &&
-            editTextPassword.text.toString().trim {it<=' '}.isNotEmpty()){
+        if(name_ET.text.toString().trim{it<=' '}.isNotEmpty() && number_ET.text.toString().trim {it<=' '}.isNotEmpty() &&
+            email_ET.text.toString().trim{it<=' '}.isNotEmpty() && password_ET.text.toString().trim {it<=' '}.isNotEmpty()){
             return true
         }
         return false
